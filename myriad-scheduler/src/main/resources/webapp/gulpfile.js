52ed3d42c760799ecfcb27569c8b6a3118d098f2
Oscarf <ofernandez@keedio.com>
2019-07-04 09:05:07 +0200

    Updated node, gradle, gulp and npm dependencies to fix latest build

--- myriad-scheduler/src/main/resources/webapp/gulpfile.js
+++ myriad-scheduler/src/main/resources/webapp/gulpfile.js
@@ -32,76 +32,76 @@ var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 var del = require('del');
 
-gulp.task("js", ['clean'], function () {
-  browserify({
-    entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
-    transform: ['babelify'],
-    debug: false,
-    fullPaths: false
-    })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(rename('myriad.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest("public/js/"));
+gulp.task('clean', function() {
+    return del(['./public']);
 });
 
-gulp.task("js-dev", ['clean'], function () {
-  browserify({
-    entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
-    transform: ['babelify'],
-    debug: true,
-    fullPaths: true
+gulp.task('js', gulp.series('clean', function(done) {
+    browserify({
+        entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
+        transform: ['babelify'],
+        debug: false,
+        fullPaths: false
     })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(rename('myriad.js'))
-    .pipe(gulp.dest("public/js/"));
-});
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(rename('myriad.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest("public/js/"));
+    done();
+}));
+
+gulp.task('js-dev', gulp.series('clean', function() {
+    browserify({
+        entries: ['./js/app.js'], // Only need initial file, browserify finds the deps
+        transform: ['babelify'],
+        debug: true,
+        fullPaths: true
+    })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(rename('myriad.js'))
+        .pipe(gulp.dest("public/js/"));
+}));
 
 
-gulp.task('html', ['clean'], function () {
+gulp.task('html', gulp.series('clean', function() {
     return gulp.src('*.html')
         .pipe( gulp.dest('public/'))
-});
+}));
 
-gulp.task('css', ['clean'], function () {
+gulp.task('css', gulp.series('clean', function() {
     return gulp.src('css/*.css')
         .pipe( gulp.dest('public/css/'))
-});
+}));
 
-gulp.task('img', ['clean'], function () {
+gulp.task('img', gulp.series('clean', function() {
     return gulp.src('img/**')
         .pipe( gulp.dest('public/img/'))
-});
+}));
 
-gulp.task('webserver', ['build-dev'], function() {
-  gulp.src('./public')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true,
-      port: 8888
-    }));
-});
+gulp.task('build-dev', gulp.series('js-dev', 'html', 'css', 'img'));
 
-gulp.task('watch', ['build-dev'], function() {
+gulp.task('webserver', gulp.series('build-dev', function() {
+    gulp.src('./public')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            port: 8888
+        }));
+}));
+
+gulp.task('watch', gulp.series('build-dev', function() {
     gulp.watch('index.html', ['html']);
     gulp.watch('css/**', ['css']);
     gulp.watch('js/**', ['js-dev']);
     gulp.watch('img/**', ['img']);
-});
+}));
 
-gulp.task('clean', function() {
-  return del(['./public']);
-});
+gulp.task('dev', gulp.series('watch', 'webserver'));
 
-gulp.task('build-dev', ['js-dev', 'html', 'css', 'img']);
+gulp.task('default', gulp.series('js', 'html', 'css', 'img'));
 
-gulp.task('dev', ['watch', 'webserver']);
-
-gulp.task('default', ['js', 'html', 'css', 'img']);
-
-gulp.task('build', ['default']); // gradle calls gulp build by default
-
+gulp.task('build', gulp.series('default'));
